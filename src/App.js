2c3d5eebd7076669c41e21feb6ab9a399b21fcbf
prathameshkhandare <prathameshkhandare15@gmail.com>
2026-06-14 11:46:29 +0530

    Fix Facebook Pixel PageView tracking on route change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import AllRoutes from "./Routes/AllRoutes";
 
-
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.fbq) {
+      window.fbq('track', 'PageView');
+    }
+  }, [location.pathname]);
+
   return (
    <>
     <AllRoutes />
